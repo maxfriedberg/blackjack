d6093bf1825999958bf5ebd789675f8641177a98
maxfriedberg <maxfried@umich.edu>
2023-09-20 23:37:00 -0400

    first run move deciding algo.

---
+++ charts.py
@@ -0,0 +1,125 @@
+hard_chart = {
+    5: {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    6: {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    7: {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    8: {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    9: {"2": "Hit", "3": "Double", "4": "Double", "5": "Double", "6": "Double", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    10: {"2": "Double", "3": "Double", "4": "Double", "5": "Double", "6": "Double", "7": "Double", "8": "Double", "9": "Double", "10": "Hit", "A": "Hit"},
+    11: {"2": "Double", "3": "Double", "4": "Double", "5": "Double", "6": "Double", "7": "Double", "8": "Double", "9": "Double", "10": "Double", "A": "Hit"},
+    12: {"2": "Hit", "3": "Hit", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    13: {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    14: {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    15: {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    16: {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    17: {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Stand", "8": "Stand", "9": "Stand", "10": "Stand", "A": "Stand"},
+    18: {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Stand", "8": "Stand", "9": "Stand", "10": "Stand", "A": "Stand"},
+    19: {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Stand", "8": "Stand", "9": "Stand", "10": "Stand", "A": "Stand"},
+    20: {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Stand", "8": "Stand", "9": "Stand", "10": "Stand", "A": "Stand"}
+}
+
+hard_chart_no_doubles = {
+    5: {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    6: {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    7: {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    8: {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    9: {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    10: {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    11: {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    12: {"2": "Hit", "3": "Hit", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    13: {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    14: {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    15: {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    16: {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    17: {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Stand", "8": "Stand", "9": "Stand", "10": "Stand", "A": "Stand"},
+    18: {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Stand", "8": "Stand", "9": "Stand", "10": "Stand", "A": "Stand"},
+    19: {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Stand", "8": "Stand", "9": "Stand", "10": "Stand", "A": "Stand"},
+    20: {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Stand", "8": "Stand", "9": "Stand", "10": "Stand", "A": "Stand"}
+}
+
+soft_chart = {
+    "A, 2": {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Double", "6": "Double", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "A, 3": {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Double", "6": "Double", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "A, 4": {"2": "Hit", "3": "Hit", "4": "Double", "5": "Double", "6": "Double", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "A, 5": {"2": "Hit", "3": "Hit", "4": "Double", "5": "Double", "6": "Double", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "A, 6": {"2": "Hit", "3": "Double", "4": "Double", "5": "Double", "6": "Double", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "A, 7": {"2": "Stand", "3": "Double", "4": "Double", "5": "Double", "6": "Double", "7": "Stand", "8": "Stand", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "A, 8": {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Stand", "8": "Stand", "9": "Stand", "10": "Stand", "A": "Stand"},
+    "A, 9": {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Stand", "8": "Stand", "9": "Stand", "10": "Stand", "A": "Stand"},
+    "2, A": {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Double", "6": "Double", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "3, A": {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Double", "6": "Double", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "4, A": {"2": "Hit", "3": "Hit", "4": "Double", "5": "Double", "6": "Double", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "5, A": {"2": "Hit", "3": "Hit", "4": "Double", "5": "Double", "6": "Double", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "6, A": {"2": "Hit", "3": "Double", "4": "Double", "5": "Double", "6": "Double", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "7, A": {"2": "Stand", "3": "Double", "4": "Double", "5": "Double", "6": "Double", "7": "Stand", "8": "Stand", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "8, A": {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Stand", "8": "Stand", "9": "Stand", "10": "Stand", "A": "Stand"},
+    "9, A": {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Stand", "8": "Stand", "9": "Stand", "10": "Stand", "A": "Stand"}
+}
+
+soft_chart_no_doubles = {
+    "A, 2": {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "A, 3": {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "A, 4": {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "A, 5": {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "A, 6": {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "A, 7": {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Stand", "8": "Stand", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "A, 8": {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Stand", "8": "Stand", "9": "Stand", "10": "Stand", "A": "Stand"},
+    "A, 9": {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Stand", "8": "Stand", "9": "Stand", "10": "Stand", "A": "Stand"},
+    "2, A": {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "3, A": {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "4, A": {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "5, A": {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "6, A": {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Hit", "6": "Hit", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "7, A": {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Stand", "8": "Stand", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "8, A": {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Stand", "8": "Stand", "9": "Stand", "10": "Stand", "A": "Stand"},
+    "9, A": {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Stand", "8": "Stand", "9": "Stand", "10": "Stand", "A": "Stand"}
+}
+
+pairs_chart = {
+    "2, 2": {"2": "Split", "3": "Split", "4": "Split", "5": "Split", "6": "Split", "7": "Split", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "3, 3": {"2": "Split", "3": "Split", "4": "Split", "5": "Split", "6": "Split", "7": "Split", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "4, 4": {"2": "Hit", "3": "Hit", "4": "Hit", "5": "Split", "6": "Split", "7": "Split", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "5, 5": {"2": "Double", "3": "Double", "4": "Double", "5": "Double", "6": "Double", "7": "Double", "8": "Double", "9": "Double", "10": "Hit", "A": "Hit"},
+    "6, 6": {"2": "Split", "3": "Split", "4": "Split", "5": "Split", "6": "Split", "7": "Hit", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "7, 7": {"2": "Split", "3": "Split", "4": "Split", "5": "Split", "6": "Split", "7": "Split", "8": "Hit", "9": "Hit", "10": "Hit", "A": "Hit"},
+    "8, 8": {"2": "Split", "3": "Split", "4": "Split", "5": "Split", "6": "Split", "7": "Split", "8": "Split", "9": "Split", "10": "Split", "A": "Split"},
+    "9, 9": {"2": "Split", "3": "Split", "4": "Split", "5": "Split", "6": "Split", "7": "Stand", "8": "Split", "9": "Split", "10": "Stand", "A": "Stand"},
+    "10, 10": {"2": "Stand", "3": "Stand", "4": "Stand", "5": "Stand", "6": "Stand", "7": "Stand", "8": "Stand", "9": "Stand", "10": "Stand", "A": "Stand"},
+    "A, A": {"2": "Split", "3": "Split", "4": "Split", "5": "Split", "6": "Split", "7": "Split", "8": "Split", "9": "Split", "10": "Split", "A": "Split"}
+}
+
+def sort_hand(hand):
+    order = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+    return order.index(hand)
+
+def make_move(hand, num_hands, dealer_upcard):
+    sorted_hand = sorted(hand, key=sort_hand)
+    num_cards = len(sorted_hand)
+    decision = ""
+
+    if num_cards == 2 and sorted_hand[0] == sorted_hand[1] and num_hands < 4:
+        parsed_hand = sorted_hand[0] + ", " + sorted_hand[1]
+        decision = pairs_chart[parsed_hand][dealer_upcard]
+    elif sorted_hand[0] == 'A' and num_cards == 2:
+        parsed_hand = sorted_hand[0] + ", " + sorted_hand[1]
+        decision = soft_chart[parsed_hand][dealer_upcard]
+    elif sorted_hand[0] == 'A' and num_cards > 2:
+        non_ace_value = 0
+        num_aces = 0
+        for card in sorted_hand:
+            if card != 'A':
+                non_ace_value += int(card)
+            else:
+                num_aces += 1
+        hard_value = non_ace_value + ((num_aces - 1) * 1)
+        parsed_hand = sorted_hand[0] + ", " + str(hard_value)
+    elif num_cards == 2:
+        parsed_hand = int(sorted_hand[0]) + int(sorted_hand[1])
+        decision = hard_chart[parsed_hand][dealer_upcard]
+    elif num_cards > 2:
+        parsed_hand = 0
+        for card in sorted_hand:
+            parsed_hand += int(card)
+        decision = hard_chart_no_doubles[parsed_hand][dealer_upcard]
+    
+    return decision
+
---
+++ testing.py
@@ -0,0 +1,16 @@
+import random
+from charts import hard_chart
+from charts import soft_chart
+from charts import pairs_chart
+from charts import sort_hand
+from charts import make_move
+
+test_hands1 = [ [ 'A', '4' ], [ '3', '10' ], [ '3', '4', '5' ] ]
+test_hands2 = [ [ 'A', 'A' ] ]
+test_hands3 = [ [ '7', '8' ], [ '7', '7', '2' ] ]
+
+
+for hand in test_hands1:
+    for upcard in range(2, 10):
+        move = make_move(hand, len(test_hands1), str(upcard))
+        print(move)
